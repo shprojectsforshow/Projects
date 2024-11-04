8b7ff689b2c9e6a8987af28d010eb3cf08c13e8e
Scott <scotteaster2014@gmail.com>
2024-11-04 11:21:41 -0600

    b

--- output.tf
+++
@@ -1,8 +0,0 @@
-output "s3_bucket_details" {
-  description = "Outputs attributes of our S3 bucket"
-  value = [
-    "Bucket Id: ${aws_s3_bucket.example.id}",
-    "Bucket ARN: ${aws_s3_bucket.example.arn}",
-    "Bucket Domain: ${aws_s3_bucket.example.bucket_domain_name}"
-  ]
-}
