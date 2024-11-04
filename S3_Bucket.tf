12909c77e014b4cf13e17ca926a0afd20c7de77d
Scott <scotteaster2014@gmail.com>
2024-11-04 11:07:33 -0600

    terraform

--- S3_Bucket.tf
+++ S3_Bucket.tf
@@ -1,40 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.73.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-  alias  = "ue1"
-  # Configuration options
-}
-
-provider "aws" {
-  region = "eu-west-1"
-  alias  = "ew1"
-  # Configuration options
-}
-
 resource "aws_s3_bucket" "example" {
-  bucket   = "scooterk-bucket"
-  provider = aws.ue1
+  bucket = var.aws_s3_bucket_name
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "example_2" {
-  bucket   = "scooterk-bucket"
-  provider = aws.ew1
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+  tags = var.aws_tagging
 }
