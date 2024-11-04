db510fc6dfef635be6bf115942ca154e73c8a860
Scott <scotteaster2014@gmail.com>
2024-11-04 11:18:28 -0600

    tf

--- provider.tf
+++ provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-# Configuration options
+  # Configuration options
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {
   description = "The aws region to deploy in."
-  type = "string"
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "aws_s3_bucket_name" {
   description = "The unique name of the AWS bucket."
-  type = "string"
-  default = "scooterk-bucket"
+  type        = string
+  default     = "scooterk-bucket"
 }
 
 variable "aws_tagging" {
